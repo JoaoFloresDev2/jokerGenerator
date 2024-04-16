f2380b3fcf96f71f9ecd37283207a8d8eeba11bb
JoaoFloresDev <joao.flores@picpay.com>
2024-04-16 15:28:37 -0300

    Adição de projeto

---
+++ jokes/jokes/SceneDelegate.swift
@@ -0,0 +1,93 @@
+//
+//  SceneDelegate.swift
+//  jokes
+//
+//  Created by João Victor  on 23/01/24.
+//
+
+import UIKit
+import JokeGenerator
+import JokeGeneratorInterface
+import CoreAPI
+import CoreAPIInterface
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        let baseDependencie = BaseDependencieInjector(api: APIFactory(), homeFactory: HomeFactory())
+        let controller = BaseController(dependencie: baseDependencie)
+        window?.rootViewController = controller
+        window?.makeKeyAndVisible()
+    }
+
+    public typealias Dependencies = HasHomeJoke & HasCoreApi
+    public class BaseDependencieInjector: Dependencies {
+        public var homeFactory: HomeJokeFactoring
+        public var api: APIFactoring
+
+        public init(api: APIFactoring, homeFactory: HomeJokeFactoring) {
+            self.api = api
+            self.homeFactory = homeFactory
+        }
+    }
+
+    class BaseController: UIViewController {
+        typealias Dependencies = HasHomeJoke & HasCoreApi
+        
+        private let dependencie: Dependencies
+        
+        init(dependencie: Dependencies) {
+            self.dependencie = dependencie
+            super.init(nibName: nil, bundle: nil)
+            view.backgroundColor = .red
+        }
+        
+        override func viewDidLoad() {
+            view.backgroundColor = .blue
+        }
+        
+        override func viewDidAppear(_ animated: Bool) {
+            let controller = dependencie.homeFactory.make(dependencieInjector: dependencie)
+            present(controller, animated: true)
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+}
+
